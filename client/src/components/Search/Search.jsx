d2cd79140397d4061cde235504c522863766dd74
ichillous <isiah.chillous@gmail.com>
2020-09-22 13:59:27 -0400

    Added Search jsx/css.

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Search.css'
 
-const Search = () => {
+const Search = (props) => {
     return (
         <div>
-            
+            <form className="search-form" onSubmit={(e) => props.onSubmit(e)}>
+                <input 
+                    className="search-input"
+                    value={props.value}
+                    name="Search"
+                    placeholder="Search"
+                    type="text"
+                    autoFocus
+                />
+            </form>
         </div>
     );
 };
