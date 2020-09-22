e63140a398489e29d59d6c09efbba9cc29dbd449
Kenneth Palces <kennypal@Kenneths-MacBook-Pro.local>
2020-09-21 21:55:55 -0400

    app component done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
 import './App.css';
+import Home from './screens/Home/Home';
+import Blogs from './screens/Blogs/Blogs';
+import BlogCreate from './screens/Blog/Blog';
+import BlogEdit from './screens/ProductEdit/ProductEdit';
+import BlogDetail from './screens/BlogDetail/BlogDetail';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/blogs" component={Blogs} />
+        <Route path="/add-blog" component={BlogCreate} />
+        <Route exact path="/blogs/:id/edit" component={BlogEdit} />
+        <Route exact path="/blogs/:id" component={BlogDetail} />
+      </Switch>
     </div>
   );
 }
